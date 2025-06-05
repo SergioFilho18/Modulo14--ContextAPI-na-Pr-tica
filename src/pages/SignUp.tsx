aac2ef3bd000f9514a98b63a84e65bc6c8ffb7ed
SergioFilho18 <barbosasergiofilh18@gmail.com>
2025-06-04 21:50:20 -0300

    feat(SignUp): add 'Change Name' button and refactor useContext usage
    - Add button to update Context data dynamically
    - Optimize useContext consumption for better performance and better
    architecture

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,10 +4,22 @@ import { Link } from "react-router-dom"
 import { Context } from '../contexts/Context';
 
 export const SignUp = () => {
-    const { name, age } = useContext(Context);
+    const { state, dispatch } = useContext(Context);
+
+    const handleChangeName = () => {
+        dispatch({
+            type: 'CHANGE_NAME',
+            payload:{
+                    name: state.user.name === 'Pedro' ? 'Sergio Fii' : 'Pedro'
+                    }
+        });
+    }
 
     return (
-        <div>Tela SignUp de {name} que tem {age} anos
+        <div>Tela SignUp de {state.user.name} que tem {state.user.age} anos
+            <button onClick={handleChangeName}>
+                Trocar nome para {state.user.name === 'Pedro' ? 'Sergio Fii' : 'Pedro'}
+                </button>
             <br/>
             <Link to="/exibir">Ir para ShowData</Link>
         </div>
